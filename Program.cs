675a7b8a0cbd2711c6fd4a831bf1f89db2812933
maikel <maikel.iani31@gmail.com>
2022-02-09 16:35:58 -0300

    crud da dio editado por mim

---
+++ Program.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Linq;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {	
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+			while (opcaoUsuario.ToUpper() != "X")
+			{
+				switch (opcaoUsuario)
+				{
+					case "1":
+						ListarSeries();
+						break;
+					case "2":
+						InserirSerie();
+						break;
+					case "3":
+						AtualizarSerie();
+						break;
+					case "4":
+						ExcluirSerie();
+						break;
+					case "5":
+						VisualizarSerie();
+						break;
+					case "6":
+						PagarFatura();
+						break;
+					case "7":
+						BuscaSerie();
+						break;
+					case "C":
+						Console.Clear();
+						break;
+					
+
+					default:
+					{
+						Console.WriteLine(opcaoUsuario+" NÃO É UMA ESCOLHA VÁLIDA, ESCOLHA NOVAMENTE!");
+						break;
+
+					}
+					
+				}
+
+				opcaoUsuario = ObterOpcaoUsuario();
+			}
+
+			Console.WriteLine("Obrigado por utilizar nossos serviços.");
+			Console.ReadLine();
+        }
+
+		
+		private static void BuscaSerie()
+		{	
+		 	System.Console.WriteLine("Digite o título procurado: ");
+		  	var titulo= Console.ReadLine().ToUpper();
+		 	
+			System.Console.WriteLine(titulo);
+			
+			Serie serie = new Serie();
+		  	var lista = repositorio.Lista();			  
+			
+			if (lista.Count() == 0)
+			{
+				System.Console.WriteLine(" Catálogo de séries está vazio!");
+			}	
+		 						
+			else 
+			{
+						
+		 		foreach(var item in lista)
+				{ 
+					
+					if (lista.Any(item => item.retornaTitulo().Equals(titulo)))
+		 			{   
+					 
+		 				System.Console.WriteLine("\n\n Filme " +titulo+ " disponível no catálogo !  \n\n");
+						break;	 
+		 			} 
+						 
+		 		 	else
+		 		 	{	
+		 		   		System.Console.WriteLine(" Título não disponível");
+				     	break;
+				 	}	 
+			    }
+			}
+		}
+
+
+		
+
+        private static void ExcluirSerie()
+		{
+			Console.Write("Digite o id da série: ");
+			int indiceSerie = int.Parse(Console.ReadLine());
+
+			repositorio.Exclui(indiceSerie);
+		}
+
+        private static void VisualizarSerie()
+		{
+			Console.Write("Digite o id da série: ");
+			int indiceSerie = int.Parse(Console.ReadLine());
+
+			var serie = repositorio.RetornaPorId(indiceSerie);
+
+			Console.WriteLine(serie);
+		}
+
+		private static void  PagarFatura()
+		{		
+				
+			string frase = "------------->> ENVIAR PIX NO VALOR 30 reais COM SUA IDENTIFICAÇÃO--> chave pix: gatonet@gmail.com";
+			System.Console.WriteLine(frase);
+		}
+		
+			
+		
+
+
+        private static void AtualizarSerie()
+		{
+			Console.Write("Digite o id da série: ");
+			int indiceSerie = int.Parse(Console.ReadLine());
+
+			
+			foreach (int i in Enum.GetValues(typeof(Genero)))
+			{
+				Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+			}
+			Console.Write("Digite o gênero entre as opções acima: ");
+			int entradaGenero = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o Título da Série: ");
+			string entradaTitulo = Console.ReadLine().ToUpper();
+
+			Console.Write("Digite o Ano de Início da Série: ");
+			int entradaAno = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite a Descrição da Série: ");
+			string entradaDescricao = Console.ReadLine();
+
+			Serie atualizaSerie = new Serie(id: indiceSerie,
+										genero: (Genero)entradaGenero,
+										titulo: entradaTitulo,
+										ano: entradaAno,
+										descricao: entradaDescricao);
+
+			repositorio.Atualiza(indiceSerie, atualizaSerie);
+		}
+
+        private static void ListarSeries()
+		{
+			Console.WriteLine("Séries disponíveis");
+
+			var lista = repositorio.Lista();
+
+			if (lista.Count == 0)
+			{
+				Console.WriteLine("Nenhuma série cadastrada.");
+				return;
+			}
+
+			foreach (var serie in lista)
+			{
+                var excluido = serie.retornaExcluido();
+                
+				Console.WriteLine("#ID {0}: - {1} {2}", serie.retornaId(), serie.retornaTitulo(), (excluido ? "*Excluído*" : ""));
+			}
+		}
+
+        private static void InserirSerie()
+		{
+			Console.WriteLine("Inserir nova série");
+
+			
+			foreach (int i in Enum.GetValues(typeof(Genero)))
+			{
+				Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+			}
+			Console.Write("Digite o gênero entre as opções acima: ");
+			int entradaGenero = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o Título da Série: ");
+			string entradaTitulo = Console.ReadLine().ToUpper();
+
+			Console.Write("Digite o Ano de Início da Série: ");
+			int entradaAno = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite a Descrição da Série: ");
+			string entradaDescricao = Console.ReadLine();
+
+			Serie novaSerie = new Serie(id: repositorio.ProximoId(),
+										genero: (Genero)entradaGenero,
+										titulo: entradaTitulo,
+										ano: entradaAno,
+										descricao: entradaDescricao);
+
+			repositorio.Insere(novaSerie);
+		}
+
+        private static string ObterOpcaoUsuario()
+		{
+			Console.WriteLine();
+			Console.WriteLine("GATONET A SUA DISPOSIÇÃO ( QUASE LEGALIZADO - CONFIA!!)");
+			Console.WriteLine("Informe a opção desejada:");
+
+			Console.WriteLine("1- Listar séries");
+			Console.WriteLine("2- Inserir nova série");
+			Console.WriteLine("3- Atualizar série");
+			Console.WriteLine("4- Excluir série");
+			Console.WriteLine("5- Visualizar série");
+			Console.WriteLine("6- Pagar fatura");
+			Console.WriteLine("7- Buscar Série por título");
+			Console.WriteLine("C- Limpar Tela");
+			Console.WriteLine("X- Sair");
+			Console.WriteLine();
+
+			string opcaoUsuario = Console.ReadLine().ToUpper();
+			Console.WriteLine();
+			return opcaoUsuario;
+		}
+    }
+}
